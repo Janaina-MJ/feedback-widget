609532bba809e4ecb7e64674f89f23871a1864a8
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2022-05-06 08:13:43 +0100

    feat: Add functionality to feedback buttons
    So that it opens a new panel depending on the type of feedback the user
    has chosen (which is stored in react states).

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
 
 import bugImageUrl from "../assets/bug.svg"
@@ -28,9 +30,16 @@ const feedbackTypes = {
     },
 }
 
-//then, using the method Object.entries(feedbackTypes) it will return an array of arrays, a vector = > [[BUG, {..content}], [IDEA, {..content}]..]
+//then later, using the method Object.entries(feedbackTypes) it will return an array of arrays, a vector = > [[BUG, {..content}], [IDEA, {..content}]..]
+
+//declare what types of feedback it is possible to return, using the keys of feedbackType object
+type feedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
+
+    //Variable to store the type of feedback the user has chosen using react-state functionality: const ['state', 'function to toggle to state chosen']
+    const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null)
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -39,22 +48,29 @@ export function WidgetForm() {
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
+            {/* If the feedback type has not been selected yet, display the buttons */}
+            {! feedbackType ? (
+                <div className="flex py-8 gap-2 w-full">
                 
-                { Object.entries(feedbackTypes).map(([key, value]) => {
-                    return (
-                        <button
-                            key={key}
-                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            //onClick={}
-                            type="button"
-                        >
-                            <img src={value.image.source} alt={value.image.alt} />
-                            <span>{value.title}</span>
-                        </button>
-                    )
-                })}
-            </div>
+                    { Object.entries(feedbackTypes).map(([key, value]) => {
+                        return (
+                            <button
+                                key={key}
+                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                                onClick={() => setFeedbackType(key as feedbackType)}
+                                type="button"
+                            >
+                                <img src={value.image.source} alt={value.image.alt} />
+                                <span>{value.title}</span>
+                            </button>
+                        )
+                    })}
+                </div>
+
+            //else a feedback type has already been selected, display a new panel:
+            ) : (
+                <p>Hello World</p>
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Designed by <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
