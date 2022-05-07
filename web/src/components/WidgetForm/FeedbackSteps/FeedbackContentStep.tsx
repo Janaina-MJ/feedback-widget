fa4cb04342c3e82c4a0eba82aa5de64e970a6e20
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2022-05-07 14:16:14 +0100

    feat: Add functionality to widget feedback form submit button
    So that prevent the default submit event and store the form information
    in a function to be used later in the Backend.

--- web/src/components/WidgetForm/FeedbackSteps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/FeedbackSteps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -14,7 +14,20 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
     //declare a state to store the screenshot taken by the user
     const [screenshot, setScreenshot] = useState<string | null>(null)
 
+    //declare a state to store the feedback comment from text area
+    const [comment, setComment] = useState('');
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+
+        console.log({
+            screenshot,
+            comment,
+        })
+    }
+
     return (
         <>
             <header>
@@ -35,10 +48,13 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea 
                     className="min-w-[304px] w-full min-h-[112px] tx-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Describe in detail what's going on..."
+
+                    //every time the user type something, take the type event and update the comment state to store the comment
+                    onChange={ event => setComment(event.target.value) }
                 />
 
                 <footer className="flex gap-2 mt-2">
@@ -50,7 +66,8 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
 
                     <button
                         type="submit"
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                        disabled={comment.length === 0}
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
                         Send feedback
                     </button>
