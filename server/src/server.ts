ef2d56b6802e0d902f8aadedd69f48d948b3bc2f
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2022-05-31 22:48:42 +0100

    feat: Add function to send the feedback via email to website developer
    So that we can send the feedback email via server side using the
    Nodemailer service, and the service was tested with MailTrap in the
    development environment.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma';
 
 const app = express();
@@ -6,6 +7,15 @@ const app = express();
 /* Middleware */
 app.use(express.json());
 /* End of Middleware */
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "8401a98f664471",
+      pass: "da8e61e89d9c4b"
+    }
+  });
 
 app.post('/feedbacks', async (req,res) => {
     const { type, comment, screenshot } = req.body;
@@ -16,6 +26,19 @@ app.post('/feedbacks', async (req,res) => {
             comment,
             screenshot,
         }
+    })
+
+    await transport.sendMail({
+        from: 'Feedback Team <hello@feedback.com>', //while in development environment, the email doesn't need to be a real email
+        to: 'Janaina MJ <janaina.marostega@hotmail.com>', //sends the feedback email to the website owner/the developer
+        subject: 'New Feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Feedback Type: ${type}</p>`,
+            `<p>Comment: ${comment}</p>`,
+            `</div>`,
+        ].join('\n')
+
     })
 
     return res.status(201).json({ data: feedback });
