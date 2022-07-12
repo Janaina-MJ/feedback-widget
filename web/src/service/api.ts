7432a8d4da7aa8b9bc52040e066d0de378b3febf
Janaina Marostega de Jesus <janaina.marostegadejesus@gmail.com>
2022-07-12 10:30:09 +0100

    chore: set environment variables to deploy

--- web/src/service/api.ts
+++ web/src/service/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333', //backend url - server
+    baseURL: import.meta.env.VITE_API_URL, //backend url - server
 });
